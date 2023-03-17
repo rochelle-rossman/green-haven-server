8bcc9aab39383a370d31086866316a023faf2cc0
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2023-03-17 11:44:47 -0500

    setup for heroku deployment

--- greenhavenapi/models/design.py
+++ greenhavenapi/models/design.py
@@ -15,8 +15,8 @@ class Design(models.Model):
       ('traditional', 'Traditional'),
       ('midcentury_modern', 'Midcentury Modern'),
     )
-    room = models.CharField(max_length=50, choices=ROOM_CHOICES)
-    style = models.CharField(max_length=50, choices=STYLE_CHOICES)
+    room = models.TextField(max_length=50, choices=ROOM_CHOICES)
+    style = models.TextField(max_length=50, choices=STYLE_CHOICES)
     image_url = models.TextField(max_length=500)
     products = models.ManyToManyField("Product", through="ProductDesign")
     description = models.TextField(max_length=1000, default="", blank=True)
