0c3df35d7c4ce16dd91c519a3494e9320da55723
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2023-03-17 12:05:00 -0500

    setup for heroku deployment

--- greenhavenapi/models/design.py
+++ greenhavenapi/models/design.py
@@ -15,8 +15,8 @@ class Design(models.Model):
       ('traditional', 'Traditional'),
       ('midcentury_modern', 'Midcentury Modern'),
     )
-    room = models.TextField(max_length=50, choices=ROOM_CHOICES)
-    style = models.TextField(max_length=50, choices=STYLE_CHOICES)
+    room = models.TextField(max_length=200, choices=ROOM_CHOICES)
+    style = models.TextField(max_length=200, choices=STYLE_CHOICES)
     image_url = models.TextField(max_length=500)
     products = models.ManyToManyField("Product", through="ProductDesign")
     description = models.TextField(max_length=1000, default="", blank=True)
--- greenhavenapi/models/user.py
+++ greenhavenapi/models/user.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    uid = models.CharField(max_length=25, unique=True)
+    uid = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     email = models.EmailField(max_length=40)
