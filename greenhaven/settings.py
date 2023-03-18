949f5ae505f98a23d42e31e6bdf39251886f0e42
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2023-03-18 09:28:23 -0500

    added deployed client url to settings

--- greenhaven/settings.py
+++ greenhaven/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://effulgent-sundae-b263f2.netlify.app'
 )
 
 MIDDLEWARE = [
